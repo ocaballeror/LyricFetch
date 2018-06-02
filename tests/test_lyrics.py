976f44f99d965f20f941723b2a3895cef1ea503c
Oscar Caballero <ocaballeror@gmail.com>
2018-06-02 23:00:21 +0200

    Complete single getlyrics tests

--- tests/test_lyrics.py
+++ tests/test_lyrics.py
@@ -4,9 +4,13 @@ Main tests module.
 import json
 import os
 import sys
+import urllib.request
+
+import eyed3
 import pytest
 
 sys.path.append('..')
+from lyrics import CONFIG
 from lyrics import Song
 from lyrics import exclude_sources
 from lyrics import get_lastfm
@@ -21,6 +25,17 @@ from lyrics import sources
 config_file = '../config.json'
 skip_lastfm = pytest.mark.skipif(not os.path.isfile(config_file),
                                  reason='No configuration file')
+
+
+@pytest.fixture
+def mp3file():
+    url = 'http://www.noiseaddicts.com/samples_1w72b820/4930.mp3'
+    filename, response = urllib.request.urlretrieve(url)
+    audiofile = eyed3.load(filename)
+    audiofile.tag = eyed3.id3.Tag()
+    audiofile.tag.save()
+    yield filename
+    os.unlink(filename)
 
 
 @pytest.fixture(scope='session')
@@ -197,24 +212,150 @@ def test_exclude_sources_section_str():
     assert newlist == sources[-2:]
 
 
-def test_getlyrics():
+def test_getlyrics_from_info():
     """
     Check that the main method can actually return a result with lyrics.
     """
     song = Song.from_info(artist="Iron maiden", title="Hallowed be thy name")
-    lyrics = get_lyrics(song)
-    assert 'hallowed be thy name' in lyrics.lower()
+    result = get_lyrics(song)
+    assert 'hallowed be thy name' in result.song.lyrics.lower()
 
 
-def test_getlyrics_dont_overwrite():
+def test_getlyrics_from_song(mp3file):
+    """
+    Check that the main method can find the lyrics for a song and write them as
+    ID3 metadata.
+    """
+    audiofile = eyed3.load(mp3file)
+    audiofile.tag.artist = 'YOB'
+    audiofile.tag.title = 'Our raw heart'
+    audiofile.tag.save()
+    song = Song.from_filename(mp3file)
+    result = get_lyrics(song)
+    assert "my restless ghost" in result.song.lyrics.lower()
+
+
+def test_getlyrics_dont_overwrite(mp3file):
     """
     Check that we skip a song if the mp3 file already has embedded lyrics.
     """
-    pass
+    audiofile = eyed3.load(mp3file)
+    placeholder = 'Some lyrics'
+    audiofile.tag.lyrics.set(placeholder)
+    audiofile.tag.save()
+    song = Song.from_filename(mp3file)
+    assert get_lyrics(song) is None
+    song = Song.from_filename(mp3file)
+    assert song.lyrics == placeholder
 
 
-def test_getlyrics_overwrite():
+def test_getlyrics_overwrite(mp3file):
     """
     Check that we can overwrite the lyrics of a song if it already has them.
     """
-    pass
+    placeholder = 'Some lyrics'
+    audiofile = eyed3.load(mp3file)
+    audiofile.tag.lyrics.set(placeholder)
+    audiofile.tag.artist = 'Baroness'
+    audiofile.tag.title = 'Eula'
+    audiofile.tag.save()
+    song = Song.from_filename(mp3file)
+    CONFIG['overwrite'] = True
+    result = get_lyrics(song)
+    assert result.song.lyrics != placeholder
+    assert "forget the taste of my own tongue" in result.song.lyrics.lower()
+
+
+def test_lyrthread_run():
+    """
+    Test the run method for the LyrThread class, which should find the lyrics
+    for a song and put the result in a Queue.
+    """
+    def raise_error(arg):
+        print('thing')
+        raise ConnectionError
+
+    # First a normal run where the function actually returns some lyrics
+    queue = Queue()
+    song = Song.from_info(artist='Avenged sevenfold', title='Demons')
+    lyr_thread = LyrThread(lambda f: 'Lyrics', song, queue)
+    lyr_thread.start()
+    result = queue.get()
+    assert isinstance(result, dict)
+    assert result['runtime'] > 0
+    assert result['lyrics'] == 'Lyrics'
+
+    # Now we test a run where some error is raised
+    lyr_thread = LyrThread(raise_error, song, queue)
+    lyr_thread.start()
+    result = queue.get()
+    assert isinstance(result, dict)
+    assert result['runtime'] > 0
+    assert result['lyrics'] == ''
+
+
+def test_getlyrics_threaded():
+    """
+    Test the `get_lyrics_threaded()` function, which should launch a pool of
+    processes to search for lyrics in all the available sources, and return the
+    result from the first one that returns valid lyrics.
+    """
+    def source_1(_):
+        time.sleep(1)
+        return 'Lyrics 1'
+
+    def source_2(_):
+        return 'Lyrics 2'
+
+    def source_3(_):
+        return ''
+
+    # source_2 is faster than source_1, so we should expect it to return lyrics
+    # first, and source_1 to not even be in the result that's returned
+    song = Song.from_info(artist='Slipknot', title='The virus of life')
+    result = get_lyrics_threaded(song, l_sources=[source_1, source_2])
+    assert song.lyrics == 'Lyrics 2'
+    assert result.song.lyrics == 'Lyrics 2'
+    assert result.source == source_2
+    assert result.runtimes[source_2] < 1
+    assert source_1 not in result.runtimes
+
+    # Now we use source_3, which is faster than source_1, but doesn't return
+    # any lyrics, so we expect the function to ignore that result and give us
+    # the lyrics from source_1
+    song = Song.from_info(artist='Power trip', title='Ruination')
+    result = get_lyrics_threaded(song, l_sources=[source_1, source_3])
+    assert song.lyrics == 'Lyrics 1'
+    assert result.song.lyrics == 'Lyrics 1'
+    assert result.source == source_1
+    assert result.runtimes[source_3] < 1
+    assert result.runtimes[source_1] >= 1
+
+    # Lastly, we try only source_3, so we should expect the result to have no
+    # lyrics, and its `source` attribute to be None.
+    song = Song.from_info('Amon amarth', 'Back on northern shores')
+    result = get_lyrics_threaded(song, l_sources=[source_3] * 4)
+    assert song.lyrics == ''
+    assert result.song.lyrics == ''
+    assert result.source is None
+    assert result.runtimes[source_3] < 1
+
+
+def test_run_mp():
+    """
+    Test `run_mp()`, which should concurrently search for the lyrics of a list
+    of songs, source by source
+    """
+    raise NotImplementedError
+
+
+def test_process_result():
+    """
+    Check that the `process_result()` function can write the lyrics to the
+    corresponding mp3 and return wheter or not they were found.
+    """
+    raise NotImplementedError
+
+
+def test_run():
+    raise NotImplementedError
