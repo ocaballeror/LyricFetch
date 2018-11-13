0fa822b91456221303dd7f0e4b0875d8ce0bd65b
Oscar Caballero <ocaballeror@gmail.com>
2018-11-13 21:07:39 +0100

    Include sample responses for all known players

--- lyricfetch/tests/sample_responses.py
+++ lyricfetch/tests/sample_responses.py
@@ -21,3 +21,70 @@ sample_response_amarok = ([
     ('tracknumber', ('i', 11)),
     ('year', ('s', '2015'))
 ],)
+
+sample_response_clementine = (
+    ('a{sv}', [
+        ('bitrate', ('i', 252)),
+        ('mpris:artUrl', ('s', 'file:///tmp/clementine-art-y30335.jpg')),
+        ('mpris:length', ('x', 1238000000)),
+        ('mpris:trackid', ('s', '/org/clementineplayer/Clementine/Track/71')),
+        ('xesam:album', ('s', '2112')),
+        ('xesam:albumArtist', ('as', ['Rush'])),
+        ('xesam:artist', ('as', ['Rush'])),
+        ('xesam:autoRating', ('i', 50)),
+        ('xesam:comment', ('as', ['Added on 29/09/2018'])),
+        ('xesam:contentCreated', ('s', '2018-09-29T17:53:41')),
+        ('xesam:discNumber', ('i', 1)),
+        ('xesam:genre', ('as', ['Progressive Rock'])),
+        ('xesam:lastUsed', ('s', '2018-11-11T22:11:16')),
+        ('xesam:title', ('s', '2112')),
+        ('xesam:trackNumber', ('i', 1)),
+        ('xesam:url', ('s', 'file:///Music/Rush/[1976] 2112/01 2112.mp3')),
+        ('xesam:useCount', ('i', 1)),
+        ('year', ('i', 1976))]),)
+
+sample_response_spotify = (
+    ('a{sv}', [
+        ('mpris:trackid', ('s', 'spotify:track:3Dja8y60xnXAJQLl1tX8UR')),
+        ('mpris:length', ('t', 312746000)),
+        ('mpris:artUrl', ('s', 'https://open.spotify.com/image/a68c1c0c6e5469a01127177933ed08cea8e81d8d')),
+        ('xesam:album', ('s', 'Process Of A New Decline')),
+        ('xesam:albumArtist', ('as', ['Gorod'])),
+        ('xesam:artist', ('as', ['Gorod'])),
+        ('xesam:autoRating', ('d', 0.14)),
+        ('xesam:discNumber', ('i', 1)),
+        ('xesam:title', ('s', 'Splinters Of Life')),
+        ('xesam:trackNumber', ('i', 6)),
+        ('xesam:url', ('s', 'https://open.spotify.com/track/3Dja8y60xnXAJQLl1tX8UR'))
+    ]),
+)
+
+sample_response_cmus = """
+status playing
+file /Music/Death/[1987] Scream Bloody Gore/07 Baptized in Blood.mp3
+duration 269
+position 2
+tag artist Death
+tag album Scream Bloody Gore
+tag title Baptized in Blood
+tag date 1987
+tag genre Death Metal
+tag tracknumber 7
+tag albumartist Death
+tag composer Death
+tag comment Added on 9/11/2017
+set aaa_mode artist
+set continue true
+set play_library true
+set play_sorted false
+set replaygain track
+set replaygain_limit true
+set replaygain_preamp 0.000000
+set repeat false
+set repeat_current false
+set shuffle false
+set softvol false
+set vol_left 0
+set vol_right 0
+"""
+
